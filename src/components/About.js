c1e6fa08e3d24894a7ae02412cbf644d05b4ca5e
James <jhiggins3231@gmail.com>
2019-11-12 08:49:45 -0500

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -27,10 +27,10 @@ export default function About() {
     <div>
     <h1 className={classes.header}>About Me</h1>
     <ul>
-    <li className={classes.list}>Hello! My name is James Higgins and I'm a Junior Full Stack Developer with a certain affinity towards React.JS. I currently live in Indianapolis, IN and spent most of my younger life here. In 2010 I decided to pack up my life and move away to join the US Navy. After spending a little over 7 years in the Navy and travelling the world I decided to step away. I thoroughly enjoyed most of my time in the Navy and am grateful for all the experiences I had. Feel free to ask me about some of them some day! In 2017 I moved back to Indianapolis in hopes of finding a job.. No such luck was to be had. So, I packed up again and moved to Manassas, VA to work as a sub-contractor for Lockheed Martin. Once I heard The Eleven Fifty Academy was accepting the GI Bill I packed my things up again and moved back to Indianapolis to enroll. My year and a half in Virginia helped me further my skillset and reintroduce me into the civilian work style/life.</li>
+    <li className={classes.list}>Hello! My name is James Higgins and I'm a Junior Full Stack Developer with a certain affinity towards React.JS. I attended the Eleven Fifty Academy JavaScript Course in the fall of 2019. I grew up in various parts of Indiana and Tennessee but will always call Indiana home. I graduated from Fishers High School in 2009 and shortly after decided to join the Navy. A brief history about my time in the Navy is shown above. I've always been more into the technical world and have really enjoyed learning to code/develop. I'm often known as the introvert of the group but once I get comfortable I'm quite the character. I have 2 pets that have been with me now for a few years. The first is a Boxer/Pitbull Mix named Layla. The second is a bearded dragon named Sobek.</li>
     <h3 className={classes.hobbies}>Hobbies and Interest</h3>
     <li className={classes.list}>Car Enthusiast. Have had my share of custom/modified cars but gave up that lifestyle temporarily to pursue this career change.</li>
-    <li className={classes.list}>Traveler. A few of my favorites: Australia, Japan, San Juan, and Guam</li>
+    <li className={classes.list}>Traveler. A few of my favorites: Australia, Japan, San Juan, and Guam. Bucket List: France, Egypt, and Eastern Australia.</li>
     <li className={classes.list}>Video Gamer.</li>
     </ul>
 </div>
